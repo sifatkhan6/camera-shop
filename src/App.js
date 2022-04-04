b671e95afd0c6b387b3913606971785474b331ce
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-04-04 18:39:55 +0800

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Review from './components/Review/Review';
+import Blog from './components/Blog/Blog'
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>this is assignment 5</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
